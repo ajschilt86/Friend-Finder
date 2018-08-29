b1442d3c18e7c57dfd0f95ad0e8641deece27f66
ajschilt <ajschilt86@gmail.com>
2018-08-29 15:49:30 -0500

    input fields adding to api page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 var app = express();
+
+
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
